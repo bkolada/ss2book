a3730fa9620526d70df00b92b6b5bb3b657c65c9
Bartlomiej Piekarski <bartlomiej.piekarski@gmail.com>
2015-10-04 00:21:40 +0200

    login and grab first page

--- ss2book/spiders/skillport.py
+++ ss2book/spiders/skillport.py
@@ -8,30 +8,38 @@ import os
 class SkillportSpider(scrapy.Spider):
     name = "skillport"
     allowed_domains = ["mobile.skillport.books24x7.com", "secure.books24x7.com"]
-    start_urls = (
-        'http://mobile.skillport.books24x7.com/login.asp',
-        #'http://mobile.skillport.books24x7.com/viewer.asp?bookid=88814',
-    )
 
-    def parse(self, response):
-        return scrapy.FormRequest.from_response(response,
-                                                formname="loginform",
-                                                formdata={
-                                                    "usr": os.environ["SS2BOOK_USERNAME"],
-                                                    "pwd": os.environ["SS2BOOK_PASSWORD"],
-                                                    "gc": os.environ["SS2BOOK_GROUPCODE"]
-                                                },
-                                                callback=self.logged_in)
+    login_url = 'https://secure.books24x7.com/express/login.asp'
+
+    def start_requests(self):
+        # post data:
+        # usr=username&pwd=password&gc=group_code&mc=1&ic=1&nojs=1&task=dologin&retpage=login.asp&app=skillport
+        form = {
+            "usr": os.environ["SS2BOOK_USERNAME"],
+            "pwd": os.environ["SS2BOOK_PASSWORD"],
+            "gc": os.environ["SS2BOOK_GROUPCODE"],
+            "mc": "1",
+            "ic": "1",
+            "nojs": "1",
+            "task": "dologin",
+            "retpage": "login.asp",
+            "app": "skillport"
+        }
+        return [scrapy.FormRequest(self.login_url,
+                                   formdata=form,
+                                   callback=self.logged_in,
+                                   headers={'Referer':'http://mobile.skillport.books24x7.com/login.asp'}
+                               )]
 
     def logged_in(self, response):
         self.logger.info("logged into skillport")
         with open("login.html", 'wb') as f:
             f.write(response.body)
-        r = response.request
-        r.replace('http://mobile.skillport.books24x7.com/viewer.asp?bookid=88814')
-        return r
 
-    def page(self, response):
+        first_page_url = 'http://mobile.skillport.books24x7.com/viewer.asp?bookid=88814&chunkid=0000000001'
+        yield scrapy.Request(first_page_url, callback=self.parse_page)
+
+    def parse_page(self, response):
         self.logger.info("jest i strona")
         with open("page.html", 'wb') as f:
             f.write(response.body)
