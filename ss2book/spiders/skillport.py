0fe8666e8d13bd62a7d1bd92701844d2629693af
Bartlomiej Piekarski <bartlomiej.piekarski@gmail.com>
2015-10-04 16:08:25 +0200

    save book to separate directory and detect end of book

--- ss2book/spiders/skillport.py
+++ ss2book/spiders/skillport.py
@@ -12,8 +12,15 @@ class SkillportSpider(scrapy.Spider):
     allowed_domains = ["mobile.skillport.books24x7.com", "secure.books24x7.com"]
     login_url = 'https://secure.books24x7.com/express/login.asp'
 
+    def __init__(self, book_id="88814", *args, **kwargs):
+        super(SkillportSpider, self).__init__(*args, **kwargs)
+        self.book_id = book_id
+        directory_path = os.path.join(settings.TMP_DIR, self.book_id)
+        if not os.path.exists(directory_path):
+            os.makedirs(directory_path)
+
     def save_response(self, response, filename):
-        file_path = os.path.join(settings.TMP_DIR, filename)
+        file_path = os.path.join(settings.TMP_DIR, self.book_id, filename)
         self.logger.debug("saving response from %s to %s" % (response.url, file_path))
         with open(file_path, "wb") as f:
             f.write(response.body)
@@ -49,7 +56,7 @@ class SkillportSpider(scrapy.Spider):
         self.save_response(response, "confirm_log_in.html")
         if 'logout' in response.body.lower():
             self.logger.info("logged into skillport")
-            first_page_url = 'http://mobile.skillport.books24x7.com/viewer.asp?bookid=88814&chunkid=0000000001'
+            first_page_url = 'http://mobile.skillport.books24x7.com/viewer.asp?bookid=%s&chunkid=0000000001' % self.book_id
             yield scrapy.Request(first_page_url, callback=self.parse_page, meta={'page_num':1})
         else:
             self.logger.critical("Crawler couldn't log into skillport. Crawling aborted")
@@ -65,13 +72,17 @@ class SkillportSpider(scrapy.Spider):
         if '<A HREF="help.asp?item=membership" BORDER="0">offlined</A>'.lower() in response.body.lower():
             self.logger.critical("Crawler couldn't get page - content is scrambled. Crawling aborted")
             return
-        page_num = response.meta.get('page_num', -1)
 
+        page_num = response.meta.get('page_num', -1)
         self.logger.info("got page")
         self.save_response(response, "page%d.html" % page_num)
 
         next_chunk = self.gen_next_chunk(response.body)
-        next_page_url = 'http://mobile.skillport.books24x7.com/viewer.asp?bookid=88814&chunkid=' + next_chunk
+        if next_chunk == "00000000-1":
+            self.logger.info("Crawler finished a book. Thank you")
+            return
+
+        next_page_url = 'http://mobile.skillport.books24x7.com/viewer.asp?bookid=%s&chunkid=%s' % (self.book_id, next_chunk)
         yield scrapy.Request(next_page_url, callback=self.parse_page, meta={'page_num':page_num+1} if page_num != -1 else {})
 
     def gen_next_chunk(self, body):
@@ -80,7 +91,7 @@ class SkillportSpider(scrapy.Spider):
         self.logger.debug("cm_payload: %s", cm_payload)
 
         ax_pattern = "var a(1|2|3|4|5) = new Array\(\d+,(\d+),\d+\)"
-        ax_list = re.findall(ax_pattern, body)[5:]
+        ax_list = re.findall(ax_pattern, body)[-5:]
         self.logger.debug("ax_list: %s", ax_list)
 
         result = ''.join([ cm_payload[int(value)] for _x, value in ax_list])
