95d21b604d4e96a62a470384010f43e12512fe13
Bartlomiej Piekarski <bartlomiej.piekarski@gmail.com>
2015-10-04 09:57:39 +0200

    spider refactor and one test

--- ss2book/spiders/skillport.py
+++ ss2book/spiders/skillport.py
@@ -6,16 +6,16 @@ import os
 from ss2book import settings
 
 
-def save_response(response, filename):
-    with open(os.path.join(settings.TMP_DIR, filename), "wb") as f:
-        f.write(response.body)
-
-
 class SkillportSpider(scrapy.Spider):
     name = "skillport"
     allowed_domains = ["mobile.skillport.books24x7.com", "secure.books24x7.com"]
-
     login_url = 'https://secure.books24x7.com/express/login.asp'
+
+    def save_response(self, response, filename):
+        file_path = os.path.join(settings.TMP_DIR, filename)
+        self.logger.debug("saving response from %s to %s" % (response.url, file_path))
+        with open(file_path, "wb") as f:
+            f.write(response.body)
 
     def start_requests(self):
         # post data:
@@ -33,21 +33,27 @@ class SkillportSpider(scrapy.Spider):
         }
         return [scrapy.FormRequest(self.login_url,
                                    formdata=form,
-                                   callback=self.confirm_log_in,
+                                   callback=self.parse_login,
                                    headers={'Referer': 'http://mobile.skillport.books24x7.com/login.asp'}
-                                   )]
+                               )]
 
-    def confirm_log_in(self, response):
-        save_response(response, "confirm_log_in.html")
-        if "b24-errortext" in response.body:
-            self.logger.critical("Crawler couldn't log into skillport. Crawling aborted")
-            return
-        else:
+    def parse_login(self, response):
+        """
+        parse login response
+
+        @url file:///tmp/ss2book/invalid_username_password.html
+        @returns items 0 0
+        @returns request 0 0
+        """
+        self.save_response(response, "confirm_log_in.html")
+        if 'logout' in response.body.lower():
             self.logger.info("logged into skillport")
             first_page_url = 'http://mobile.skillport.books24x7.com/viewer.asp?bookid=88814&chunkid=0000000001'
             yield scrapy.Request(first_page_url, callback=self.parse_page)
+        else:
+            self.logger.critical("Crawler couldn't log into skillport. Crawling aborted")
 
     def parse_page(self, response):
-        self.logger.info("jest i strona")
-        save_response(response, "page.html")
+        self.logger.info("got page")
+        self.save_response(response, "page.html")
         return None
